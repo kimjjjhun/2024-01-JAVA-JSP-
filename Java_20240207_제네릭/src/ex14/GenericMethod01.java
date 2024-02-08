017bf8b50d4eb8af393c1ab3f1b740f693716948
kimjjjjhun <kjhun2727@gmail.com>
2024-02-08 18:00:14 +0900

    test

--- Java_20240207_제네릭/src/ex14/GenericMethod01.java
+++ Java_20240207_제네릭/src/ex14/GenericMethod01.java
@@ -1,15 +1,15 @@
 package ex14;
 
-class Box<T>{   //T --> String
-	private T obj;
+class Box<T>{   //T --> String         // 클래스 박스를 제네릭메소드로 선언
+	private T obj;                     // 접근제한자 private obj 초기값 설정 클래스 내에서만 사용할수있게 하기위해서
 	
-	void set(T obj){ 
-		this.obj = obj; }
+	void set(T obj){                   // 박스값안에 obj값을 넣는다. 라는 문장
+		this.obj = obj; }              // obj값은 박스클래스안에 있는 제네릭 obj 값이다 라고 좌표를 찍어줌
 	
-	T get() {
+	T get() {                          // 멤버변수를 리턴시킨다.   세터에서 멤버변수값을 초기화했을때 멤버변수를 리턴시켜준다.
 		return this.obj;}
 		
-		public String toString() {
+		public String toString() {       //문자열로 만들어 리턴시킨다.
 			return obj.toString();
 	}
 }
@@ -23,6 +23,8 @@ class A {
 class Unboxer{       
              // super Integer -> Integer 이거나 Integer 상위클래스면 ok
 	         // Integer -> Number -> Object
+	         // extends Integer -> Integer 이거나 Integer 하위클래스면 ok
+	         // Integer -> 자료형배열(double , float , long , int , short , byte)
 	
 	public static void peekBox(Box<? super Integer> box) {    // 와일드카드
 		System.out.println(box);
